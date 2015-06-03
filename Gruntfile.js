b3690377eacd335c991b084efe959f093fc819c3
giisyu <hasegawatann@gmail.com>
2015-06-03 10:25:10 +0900

    start grunt project

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    watch: {
+      elm: {
+        files: ["signalCatalog.elm"],
+        tasks: ["elm"]
+      }
+    },
+
+
+    elm: {
+      dev :{
+        files: {
+          "./elm.js": "./signalCatalog.elm"
+        }
+      }
+    },
+
+    browserSync: {
+      dev: {
+          options: {
+          files : "signalCatalog.elm",
+          watchTask: true,
+          port: 8000,
+          server:{
+            baseDir : "./",
+            index : "index.html"
+          }
+        }
+      }
+    },
+
+  });
+
+  ["grunt-contrib-watch" , "grunt-elm" , "grunt-browser-sync"].forEach(function(plugin) {
+    grunt.loadNpmTasks(plugin);
+  });
+
+  grunt.registerTask("default", [ "elm", "browserSync:dev", "watch"]);
+};
